8e780686e30548c7055cb281f830fdc5f293c8a0
Erik Guzman <erik.guzman@guzman.codes>
2020-09-23 21:19:28 -0700

    Update setting menu test for mobile

--- src/components/display-settings-menu/settings/__tests__/SettingsMenu.test.js
+++ src/components/display-settings-menu/settings/__tests__/SettingsMenu.test.js
@@ -8,30 +8,30 @@ afterEach(cleanup)
 describe('SettingMenu', () => {
   describe('not video overlay', () => {
     it('render expected menu items', () => {
-      const { queryByText } = renderWithRedux(
-        <SettingMenu />,
-      )
+      const { queryByText } = renderWithRedux(<SettingMenu />)
 
       expect(queryByText('Reset Position')).not.toBeInTheDocument()
       expect(queryByText('Decrease Line Count')).not.toBeInTheDocument()
       expect(queryByText('Increase Line Count')).not.toBeInTheDocument()
       expect(queryByText('Enable Square Text Box')).not.toBeInTheDocument()
+      expect(queryByText('Advanced Settings')).not.toBeInTheDocument()
       expect(queryByText('Small Text')).toBeInTheDocument()
       expect(queryByText('Medium Text')).toBeInTheDocument()
       expect(queryByText('Large Text')).toBeInTheDocument()
-      expect(queryByText('Advanced Settings')).toBeInTheDocument()
     })
   })
 
   describe('is video overlay', () => {
     beforeEach(() => {
-      window.history.pushState({}, 'Test Title', '/test.html?anchor=video_overlay&platform=web')
+      window.history.pushState(
+        {},
+        'Test Title',
+        '/test.html?anchor=video_overlay&platform=web',
+      )
     })
 
     it('renders reset button', () => {
-      const { queryByText } = renderWithRedux(
-        <SettingMenu />,
-      )
+      const { queryByText } = renderWithRedux(<SettingMenu />)
 
       expect(queryByText('Reset Position')).toBeInTheDocument()
       expect(queryByText('Enable Square Text Box')).toBeInTheDocument()
